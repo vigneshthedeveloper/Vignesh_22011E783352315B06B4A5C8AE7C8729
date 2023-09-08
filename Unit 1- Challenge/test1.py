e9b525e6a81b9272073e90d3e75b2e8155aa6ec0
Vignesh <115334523+vigneshthedeveloper@users.noreply.github.com>
2023-09-08 18:18:07 +0530

    Add files via upload

---
+++ Unit 1- Challenge/test1.py
@@ -0,0 +1,13 @@
+#FACTORIAL
+
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+result = factorial(num)
+
+print("The factorial of {} is {}".format(num, result))
